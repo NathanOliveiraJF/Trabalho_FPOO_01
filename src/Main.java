f99acc0e40d83c7f067087a699b9faac4a56e53e
Breno Reis <breno.dk@gmail.com>
2020-05-02 10:41:07 -0300

    Criação do primeiro menu

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,34 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Campeonato c = new Campeonato();
+        Scanner read = new Scanner(System.in);
+
+        System.out.println("╭───────────────────────────────────────────────╮");
+        System.out.println("│              Campeonato de Fifa               │");
+        System.out.println("╰───────────────────────────────────────────────╯");
+
+        printMenu();
+        askForOption();
+
+        int option = read.nextInt();
+        read.nextLine();
+
+        switch (option) {
+            case 1:
+                cadastraParticipante(c);
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 0:
+                System.exit(0);
+                break;
+        }
 
         Participante breno = new Participante("Breno");
         breno.setTime(c.getTime(0));
@@ -25,5 +50,22 @@ public class Main {
         System.out.println(c.listTimesDisponiveis());
 
 	    // Chama o metodo que popula a lista de times.
+    }
+
+    private static void printMenu() {
+        System.out.println("╭───────────────────────────────────────────────╮");
+        System.out.println("│ [1] Cadastrar participantes                   │");
+        System.out.println("│ [2] Iniciar campeonato                        │");
+        System.out.println("│ [3] Imprimir ranking                          │");
+        System.out.println("│ [0] Sair do programa                          │");
+        System.out.println("╰───────────────────────────────────────────────╯");
+    }
+
+    private static void askForOption() {
+        System.out.print("Escolha: ");
+    }
+
+    private static void cadastraParticipante(Campeonato c) {
+        c.cadastraParticipante(new Participante("Breno"));
     }
 }
